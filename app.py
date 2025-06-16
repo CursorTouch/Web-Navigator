4cc2f4af6e35eb1fcec8ceb36341255e53aa99b7
Jeomon <jeogeoalukka@gmail.com>
2025-06-16 23:17:42 +0530

    added thinking tool

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from src.agent.web.browser.config import BrowserConfig
 from src.inference.gemini import ChatGemini
+from src.tool.thinking import thinking_tool
 from src.agent.web import WebAgent
 from dotenv import load_dotenv
 import os
@@ -11,8 +12,8 @@ browser_instance_dir = os.getenv('BROWSER_INSTANCE_DIR')
 user_data_dir = os.getenv('USER_DATA_DIR')
 
 llm=ChatGemini(model='gemini-2.0-flash', api_key=google_api_key, temperature=0)
-config=BrowserConfig(device='iPhone 13',browser='edge',browser_instance_dir=browser_instance_dir,user_data_dir=user_data_dir,headless=False)
-agent = WebAgent(config=config,llm=llm,verbose=True,use_vision=True,max_iteration=100,include_human_in_loop=False,token_usage=False)
+config=BrowserConfig(device=None,browser='edge',browser_instance_dir=browser_instance_dir,user_data_dir=user_data_dir,headless=False)
+agent = WebAgent(config=config, additional_tools=[thinking_tool],llm=llm,verbose=True,use_vision=True,max_iteration=100,include_human_in_loop=False,token_usage=False)
 
 user_query = input('Enter your query: ')
 agent_response = agent.invoke(user_query)
